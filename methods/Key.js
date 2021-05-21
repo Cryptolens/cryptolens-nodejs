9cfe100a9570c6f06c66d259366a36a77a786be5
Artem Los <arteml@kth.se>
2021-05-21 09:48:45 +0200

    Add GetKey

--- methods/Key.js
+++ methods/Key.js
@@ -1,12 +1,37 @@
 const request = require('request');
 const helpers = require('../internal/HelperMethods.js');
-const pubHelpers = require('./Helpers.js');
 
 module.exports = class Key {
+    
     static Activate(token, rsaPubKey, ProductId, Key, MachineCode = "", FieldsToReturn = 0, Metadata=false, FloatingTimeInterval = 0, MaxOverdraft = 0) {
 
         return new Promise((resolve, reject) => {
             request(`https://app.cryptolens.io/api/key/Activate?token=${token}&productId=${ProductId}&Key=${Key}&machineCode=${MachineCode}&fieldsToReturn=${FieldsToReturn}&Metadata=${Metadata}&FloatingTimeInterval=${FloatingTimeInterval}&MaxOverdraft=${MaxOverdraft}&Sign=true&SignMethod=1`, { json: true }, (err, res, body) => {
+                if (err || body.result == "1") {
+                    console.warn(err);
+                    if(!err) {
+                        console.warn(body.message);
+                    }
+                    resolve(null);
+                } else {
+                    if(helpers.VerifySignature(body, rsaPubKey)) {
+                        var license = JSON.parse(Buffer.from(body["licenseKey"], "base64").toString("utf-8"));
+                        license.RawResponse = body;
+                        resolve(license);
+                    } else{
+                        console.warn("Signature verification failed.");
+                        resolve(null);
+                    }
+                }
+            });
+        });
+      
+    }
+
+    static GetKey(token, rsaPubKey, ProductId, Key, FieldsToReturn = 0, Metadata=false) {
+
+        return new Promise((resolve, reject) => {
+            request(`https://app.cryptolens.io/api/key/GetKey?token=${token}&productId=${ProductId}&Key=${Key}&fieldsToReturn=${FieldsToReturn}&Metadata=${Metadata}&Sign=true&SignMethod=1`, { json: true }, (err, res, body) => {
                 if (err || body.result == "1") {
                     console.warn(err);
                     if(!err) {
--- test.js
+++ test.js
@@ -2,7 +2,9 @@ var Key = require("./methods/Key.js")
 const Helpers = require('./methods/Helpers.js');
 
 var RSAPubKey = "<RSAKeyValue><Modulus>sGbvxwdlDbqFXOMlVUnAF5ew0t0WpPW7rFpI5jHQOFkht/326dvh7t74RYeMpjy357NljouhpTLA3a6idnn4j6c3jmPWBkjZndGsPL4Bqm+fwE48nKpGPjkj4q/yzT4tHXBTyvaBjA8bVoCTnu+LiC4XEaLZRThGzIn5KQXKCigg6tQRy0GXE13XYFVz/x1mjFbT9/7dS8p85n8BuwlY5JvuBIQkKhuCNFfrUxBWyu87CFnXWjIupCD2VO/GbxaCvzrRjLZjAngLCMtZbYBALksqGPgTUN7ZM24XbPWyLtKPaXF2i4XRR9u6eTj5BfnLbKAU5PIVfjIS+vNYYogteQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
-var result = Key.Activate(token="WyIzMzY0IiwiZHRGd1N0Vm0zUUY0c3p6MGRjbllGRmgxUGlSblVFeUw0NHBLTHd1SSJd", RSAPubKey, ProductId=3349, Key="GEBNC-WZZJD-VJIHG-GCMVD", MachineCode="test");
 
+var result = Key.Activate(token="", RSAPubKey, ProductId=3349, Key="GEBNC-WZZJD-VJIHG-GCMVD", MachineCode="test");
 result.then((s)=> console.log(Helpers.LoadFromString(RSAPubKey, Helpers.SaveAsString(s))));
 
+var result2 = Key.GetKey(token="", RSAPubKey, ProductId=3349, Key="GEBNC-WZZJD-VJIHG-GCMVD");
+result2.then((s)=> console.log(Helpers.LoadFromString(RSAPubKey, Helpers.SaveAsString(s))));
