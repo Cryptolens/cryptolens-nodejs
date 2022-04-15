250766d238e59b0943e320fcc45603b16fc5f662
Md Al Amin <alaminopu.me@gmail.com>
2022-04-15 17:26:38 +0200

    Replace depricated request library with got

--- methods/Key.js
+++ methods/Key.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const got = require('got');
 const helpers = require('../internal/HelperMethods.js');
 
 module.exports = class Key {
@@ -6,24 +6,26 @@ module.exports = class Key {
     static Activate(token, rsaPubKey, ProductId, Key, MachineCode = "", FieldsToReturn = 0, Metadata = false, FloatingTimeInterval = 0, MaxOverdraft = 0) {
 
         return new Promise((resolve, reject) => {
-            request(`https://api.cryptolens.io/api/key/Activate?token=${token}&productId=${ProductId}&Key=${Key}&machineCode=${MachineCode}&fieldsToReturn=${FieldsToReturn}&Metadata=${Metadata}&FloatingTimeInterval=${FloatingTimeInterval}&MaxOverdraft=${MaxOverdraft}&Sign=true&SignMethod=1`, { json: true }, (err, res, body) => {
-                if (err || body.result == "1") {
-                    console.warn(err);
-                    if (!err) {
+            (async () => {
+                try{
+                    const body = await got.post(`https://api.cryptolens.io/api/key/Activate?token=${token}&productId=${ProductId}&Key=${Key}&machineCode=${MachineCode}&fieldsToReturn=${FieldsToReturn}&Metadata=${Metadata}&FloatingTimeInterval=${FloatingTimeInterval}&MaxOverdraft=${MaxOverdraft}&Sign=true&SignMethod=1`).json();
+                    if (body.result == "1") {
                         console.warn(body.message);
-                    }
-                    resolve(null);
-                } else {
-                    if (helpers.VerifySignature(body, rsaPubKey)) {
-                        var license = JSON.parse(Buffer.from(body["licenseKey"], "base64").toString("utf-8"));
-                        license.RawResponse = body;
-                        resolve(license);
-                    } else {
-                        console.warn("Signature verification failed.");
                         resolve(null);
+                    } else {
+                        if (helpers.VerifySignature(body, rsaPubKey)) {
+                            var license = JSON.parse(Buffer.from(body["licenseKey"], "base64").toString("utf-8"));
+                            license.RawResponse = body;
+                            resolve(license);
+                        } else {
+                            console.warn("Signature verification failed.");
+                            resolve(null);
+                        }
                     }
-                }
-            });
+                } catch(error) {
+                    reject(error);
+                };
+            })();
         });
 
     }
@@ -31,18 +33,20 @@ module.exports = class Key {
     static Deactivate(token, ProductId, Key, MachineCode = "", Floating = false, OSInfo = null) {
 
         return new Promise((resolve, reject) => {
-            request(`https://api.cryptolens.io/api/key/Deactivate?token=${token}&productId=${ProductId}&Key=${Key}&machineCode=${MachineCode}&floating=${Floating}&OSInfo=${OSInfo}`, { json: true }, (err, res, body) => {
-                if (err || body.result == "1") {
-                    console.warn(err);
-                    if (!err) {
+            (async () => {
+                try{
+                    const body = got(`https://api.cryptolens.io/api/key/Deactivate?token=${token}&productId=${ProductId}&Key=${Key}&machineCode=${MachineCode}&floating=${Floating}&OSInfo=${OSInfo}`);
+                    if (body.result == "1") {
                         console.warn(body.message);
+                        resolve(null);
+                    } else {
+                        console.warn(body.message);
+                        resolve(null);
                     }
-                    resolve(null);
-                } else {
-                    console.warn(body.message);
-                    resolve(null);
+                }catch(error){
+                    reject(error)
                 }
-            });
+            })();
         });
 
     }
@@ -50,24 +54,26 @@ module.exports = class Key {
     static GetKey(token, rsaPubKey, ProductId, Key, FieldsToReturn = 0, Metadata = false) {
 
         return new Promise((resolve, reject) => {
-            request(`https://api.cryptolens.io/api/key/GetKey?token=${token}&productId=${ProductId}&Key=${Key}&fieldsToReturn=${FieldsToReturn}&Metadata=${Metadata}&Sign=true&SignMethod=1`, { json: true }, (err, res, body) => {
-                if (err || body.result == "1") {
-                    console.warn(err);
-                    if (!err) {
+            (async () => {
+                try{
+                    const body = got(`https://api.cryptolens.io/api/key/GetKey?token=${token}&productId=${ProductId}&Key=${Key}&fieldsToReturn=${FieldsToReturn}&Metadata=${Metadata}&Sign=true&SignMethod=1`);
+                    if (body.result == "1") {
                         console.warn(body.message);
-                    }
-                    resolve(null);
-                } else {
-                    if (helpers.VerifySignature(body, rsaPubKey)) {
-                        var license = JSON.parse(Buffer.from(body["licenseKey"], "base64").toString("utf-8"));
-                        license.RawResponse = body;
-                        resolve(license);
-                    } else {
-                        console.warn("Signature verification failed.");
                         resolve(null);
+                    } else {
+                        if (helpers.VerifySignature(body, rsaPubKey)) {
+                            var license = JSON.parse(Buffer.from(body["licenseKey"], "base64").toString("utf-8"));
+                            license.RawResponse = body;
+                            resolve(license);
+                        } else {
+                            console.warn("Signature verification failed.");
+                            resolve(null);
+                        }
                     }
+                }catch(error){
+                    reject(error)
                 }
-            });
+            })();
         });
 
     }
