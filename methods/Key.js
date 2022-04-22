e961ae9465a47d2ab9deea149750d5ade1e905bc
roelvanegmond <36486184+roelvanegmond@users.noreply.github.com>
2022-04-22 13:50:13 +0200

    Added support for custom apiURL

--- methods/Key.js
+++ methods/Key.js
@@ -1,80 +1,90 @@
-const got = require('got');
-const helpers = require('../internal/HelperMethods.js');
+const got = require("got");
+const helpers = require("../internal/HelperMethods.js");
 
 module.exports = class Key {
+  static Activate(
+    token,
+    rsaPubKey,
+    ProductId,
+    Key,
+    MachineCode = "",
+    FieldsToReturn = 0,
+    Metadata = false,
+    FloatingTimeInterval = 0,
+    MaxOverdraft = 0,
+    apiURL = "https://api.cryptolens.io"
+  ) {
+    return new Promise((resolve, reject) => {
+      (async () => {
+        try {
+          const body = await got(
+            `${apiURL}/api/key/Activate?token=${token}&productId=${ProductId}&Key=${Key}&machineCode=${MachineCode}&fieldsToReturn=${FieldsToReturn}&Metadata=${Metadata}&FloatingTimeInterval=${FloatingTimeInterval}&MaxOverdraft=${MaxOverdraft}&Sign=true&SignMethod=1`
+          ).json();
+          if (body.result == "1") {
+            console.warn(body.message);
+            resolve(null);
+          } else {
+            if (helpers.VerifySignature(body, rsaPubKey)) {
+              var license = JSON.parse(Buffer.from(body["licenseKey"], "base64").toString("utf-8"));
+              license.RawResponse = body;
+              resolve(license);
+            } else {
+              console.warn("Signature verification failed.");
+              resolve(null);
+            }
+          }
+        } catch (error) {
+          reject(error);
+        }
+      })();
+    });
+  }
 
-    static Activate(token, rsaPubKey, ProductId, Key, MachineCode = "", FieldsToReturn = 0, Metadata = false, FloatingTimeInterval = 0, MaxOverdraft = 0) {
+  static Deactivate(token, ProductId, Key, MachineCode = "", Floating = false, OSInfo = null, apiURL = "https://api.cryptolens.io") {
+    return new Promise((resolve, reject) => {
+      (async () => {
+        try {
+          const body = got(
+            `${apiURL}/api/key/Deactivate?token=${token}&productId=${ProductId}&Key=${Key}&machineCode=${MachineCode}&floating=${Floating}&OSInfo=${OSInfo}`
+          );
+          if (body.result == "1") {
+            console.warn(body.message);
+            resolve(null);
+          } else {
+            console.warn(body.message);
+            resolve(null);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      })();
+    });
+  }
 
-        return new Promise((resolve, reject) => {
-            (async () => {
-                try{
-                    const body = await got(`https://api.cryptolens.io/api/key/Activate?token=${token}&productId=${ProductId}&Key=${Key}&machineCode=${MachineCode}&fieldsToReturn=${FieldsToReturn}&Metadata=${Metadata}&FloatingTimeInterval=${FloatingTimeInterval}&MaxOverdraft=${MaxOverdraft}&Sign=true&SignMethod=1`).json();
-                    if (body.result == "1") {
-                        console.warn(body.message);
-                        resolve(null);
-                    } else {
-                        if (helpers.VerifySignature(body, rsaPubKey)) {
-                            var license = JSON.parse(Buffer.from(body["licenseKey"], "base64").toString("utf-8"));
-                            license.RawResponse = body;
-                            resolve(license);
-                        } else {
-                            console.warn("Signature verification failed.");
-                            resolve(null);
-                        }
-                    }
-                } catch(error) {
-                    reject(error);
-                };
-            })();
-        });
-
-    }
-
-    static Deactivate(token, ProductId, Key, MachineCode = "", Floating = false, OSInfo = null) {
-
-        return new Promise((resolve, reject) => {
-            (async () => {
-                try{
-                    const body = got(`https://api.cryptolens.io/api/key/Deactivate?token=${token}&productId=${ProductId}&Key=${Key}&machineCode=${MachineCode}&floating=${Floating}&OSInfo=${OSInfo}`);
-                    if (body.result == "1") {
-                        console.warn(body.message);
-                        resolve(null);
-                    } else {
-                        console.warn(body.message);
-                        resolve(null);
-                    }
-                }catch(error){
-                    reject(error)
-                }
-            })();
-        });
-
-    }
-
-    static GetKey(token, rsaPubKey, ProductId, Key, FieldsToReturn = 0, Metadata = false) {
-
-        return new Promise((resolve, reject) => {
-            (async () => {
-                try{
-                    const body = got(`https://api.cryptolens.io/api/key/GetKey?token=${token}&productId=${ProductId}&Key=${Key}&fieldsToReturn=${FieldsToReturn}&Metadata=${Metadata}&Sign=true&SignMethod=1`);
-                    if (body.result == "1") {
-                        console.warn(body.message);
-                        resolve(null);
-                    } else {
-                        if (helpers.VerifySignature(body, rsaPubKey)) {
-                            var license = JSON.parse(Buffer.from(body["licenseKey"], "base64").toString("utf-8"));
-                            license.RawResponse = body;
-                            resolve(license);
-                        } else {
-                            console.warn("Signature verification failed.");
-                            resolve(null);
-                        }
-                    }
-                }catch(error){
-                    reject(error)
-                }
-            })();
-        });
-
-    }
-}
+  static GetKey(token, rsaPubKey, ProductId, Key, FieldsToReturn = 0, Metadata = false, apiURL = "https://api.cryptolens.io") {
+    return new Promise((resolve, reject) => {
+      (async () => {
+        try {
+          const body = got(
+            `${apiURL}/api/key/GetKey?token=${token}&productId=${ProductId}&Key=${Key}&fieldsToReturn=${FieldsToReturn}&Metadata=${Metadata}&Sign=true&SignMethod=1`
+          );
+          if (body.result == "1") {
+            console.warn(body.message);
+            resolve(null);
+          } else {
+            if (helpers.VerifySignature(body, rsaPubKey)) {
+              var license = JSON.parse(Buffer.from(body["licenseKey"], "base64").toString("utf-8"));
+              license.RawResponse = body;
+              resolve(license);
+            } else {
+              console.warn("Signature verification failed.");
+              resolve(null);
+            }
+          }
+        } catch (error) {
+          reject(error);
+        }
+      })();
+    });
+  }
+};
