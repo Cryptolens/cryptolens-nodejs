ba9a5fcaf167b49b9bc38896d294f2ac3680f91a
Artem Los <arteml@kth.se>
2022-04-26 10:01:55 +0200

    Update Key.js

--- methods/Key.js
+++ methods/Key.js
@@ -5,12 +5,22 @@ module.exports = class Key {
 
     static Activate(token, rsaPubKey, ProductId, Key, MachineCode = "", FieldsToReturn = 0, Metadata = false, FloatingTimeInterval = 0, MaxOverdraft = 0, LicenseServerUrl = "https://api.cryptolens.io") {
 
-        console.log(LicenseServerUrl);
         return new Promise((resolve, reject) => {
             (async () => {
                 try{
                     const body = await got.post(`${LicenseServerUrl}/api/key/Activate`, {
-                        body: `token=${encodeURIComponent(token)}&ProductId=${encodeURIComponent(ProductId)}&Key=${encodeURIComponent(Key)}&MachineCode=${encodeURIComponent(MachineCode)}&FieldsToReturn=${FieldsToReturn}&Metadata=${encodeURIComponent(Metadata)}&FloatingTimeInterval=${encodeURIComponent(FloatingTimeInterval)}&MaxOverdraft=${encodeURIComponent(MaxOverdraft)}&Sign=true&SignMethod=1`
+                        form: {
+                            token : token,
+                            ProductId : ProductId,
+                            Key : Key,
+                            MachineCode : MachineCode,
+                            FieldsToReturn : FieldsToReturn,
+                            Metadata : Metadata,
+                            FloatingTimeInterval : FloatingTimeInterval,
+                            MaxOverdraft: MaxOverdraft,
+                            Sign: true,
+                            SignMethod : 1
+                        }
                     }).json();
 
                     if (body.result == "1") {
@@ -41,8 +51,16 @@ module.exports = class Key {
                 try{
 
                     const body = await got.post(`${LicenseServerUrl}/api/key/Deactivate`, {
-                        body: `token=${encodeURIComponent(token)}&ProductId=${encodeURIComponent(ProductId)}&Key=${encodeURIComponent(Key)}&MachineCode=${encodeURIComponent(MachineCode)}&Floating=${encodeURIComponent(Floating)}&OSInfo=${encodeURIComponent(OSInfo)}`
+                        form: {
+                            token : token,
+                            ProductId : ProductId,
+                            Key : Key,
+                            MachineCode : MachineCode,
+                            Floating: Floating,
+                            OSInfo : OSInfo
+                        }
                     }).json();
+
 
                     if (body.result == "1") {
                         console.warn(body.message);
@@ -65,8 +83,18 @@ module.exports = class Key {
             (async () => {
                 try{
                     const body = await got.post(`${LicenseServerUrl}/api/key/GetKey`, {
-                        body: `token=${encodeURIComponent(token)}&ProductId=${encodeURIComponent(ProductId)}&Key=${encodeURIComponent(Key)}&MachineCode=${encodeURIComponent(MachineCode)}&FieldsToReturn=${FieldsToReturn}&Metadata=${encodeURIComponent(Metadata)}`
+                        form: {
+                            token : token,
+                            ProductId : ProductId,
+                            Key : Key,
+                            MachineCode : MachineCode,
+                            FieldsToReturn : FieldsToReturn,
+                            Metadata : Metadata,
+                            Sign: true,
+                            SignMethod : 1
+                        }
                     }).json();
+
 
                     if (body.result == "1") {
                         console.warn(body.message);
