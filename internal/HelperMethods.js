f757536469f855c99e308380e8c1c7b340ceba6e
Artem Los <artem@cryptolens.io>
2022-08-09 13:58:43 +0200

    Catch errors when the server reply is not JSON (#22)
    * Update HelperMethods.js
    
    * Update
    
    Co-Authored-By: Jorn Verhoeven <jorn.j.verhoeven@gmail.com>
    
    * Update HelperMethods.js
    
    Co-Authored-By: Jorn Verhoeven <jorn.j.verhoeven@gmail.com>
    
    Co-authored-by: Jorn Verhoeven <jorn.j.verhoeven@gmail.com>

--- internal/HelperMethods.js
+++ internal/HelperMethods.js
@@ -43,7 +43,13 @@ module.exports = class HelperMethods {
                     }
                 } catch(error){
                     if (error.name === "HTTPError") {
-                        reject(new Error(JSON.parse(error.response.body).message));
+                        try {
+                            reject(new Error(JSON.parse(error.response.body).message));
+                        } catch (e) {
+                            reject(new Error("Could not parse the error message returned from the server. "+
+                            "If this error persists, please check that the LicenseServerUrl does not end with '/'. "+
+                            "E.g. 'https://api.cryptolens.io' is correct whereas 'https://api.cryptolens.io/' is not."));
+                        }
                     } else {
                         reject(error);
                     }
